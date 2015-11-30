1fe101f5d590e84ae48c48999758c5ce8a0a2858
Suhendro <suhendro.mail@gmail.com>
2015-11-30 10:36:33 +0700

    - add grunt related plugin, including Gruntfile.js - add minified files of css and js

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+/**
+ * grunt-pagespeed-ngrok
+ * http://www.jamescryer.com/grunt-pagespeed-ngrok
+ *
+ * Copyright (c) 2014 James Cryer
+ * http://www.jamescryer.com
+ */
+//'use strict'
+
+var ngrok = require('ngrok');
+
+module.exports = function(grunt) {
+
+  // Load grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Grunt configuration
+  grunt.initConfig({
+    uglify: {
+        my_target: {
+          files: [{
+              expand: true,
+              cwd: 'js',
+              src: ['*.js', '!*.min.js'],
+              dest: 'js',
+              ext: '.min.js'
+          }]
+        }
+      },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    pagespeed: {
+      options: {
+        nokey: true,
+        locale: "en_GB",
+        threshold: 40
+      },
+      local: {
+        options: {
+          strategy: "desktop"
+        }
+      },
+      mobile: {
+        options: {
+          strategy: "mobile"
+        }
+      }
+    }
+  });
+
+  // Register customer task for ngrok
+  grunt.registerTask('psi-ngrok', 'Run pagespeed with ngrok', function() {
+    var done = this.async();
+    var port = 9292;
+
+    ngrok.connect(port, function(err, url) {
+      if (err !== null) {
+        grunt.fail.fatal(err);
+        return done();
+      }
+      grunt.config.set('pagespeed.options.url', url);
+      grunt.task.run('pagespeed');
+      done();
+    });
+  });
+    
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Register default tasks
+  grunt.registerTask('default', ['psi-ngrok']);
+}
